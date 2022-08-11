838ce451174923f0cd276d51a0700f7c6d5729fc
MoAyoub <medayb45esi@gmail.com>
2022-08-11 18:24:32 +0100

    add option

--- components/products/Box.jsx
+++ components/products/Box.jsx
@@ -1,8 +1,14 @@
-export default function Box({ styles, name, id }) {
+export default function Box({ styles, option, onChange }) {
   return (
     <div className={styles.option}>
-      <input className={styles.checkbox} type="checkbox" id={id} name={id} />
-      <label htmlFor={id}>{name}</label>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        id={option.text}
+        name={option.text}
+        onChange={(e) => onChange(e, option.price)}
+      />
+      <label htmlFor={option.text}>{option.text}</label>
     </div>
   );
 }
--- components/products/Right.jsx
+++ components/products/Right.jsx
@@ -4,11 +4,24 @@ import { useState } from "react";
 export default function Right({ styles, pizza }) {
   const taille = ["Small", "Medium", "Large"];
   const [size, setSize] = useState(0);
+  const [price, setPrice] = useState(pizza.prices[0]);
+  const changePrice = (number) => {
+    setPrice(price + number);
+  };
+  const handleChangeSize = (sizeIndex) => {
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size];
+    setSize(sizeIndex);
+    changePrice(difference);
+  };
+  const handleChange = (e, option) => {
+    const { checked } = e.target;
+    changePrice(checked ? option : -option);
+  };
 
   return (
     <div className={styles.right}>
       <h1 className={styles.title}>{pizza.title}</h1>
-      <span className={styles.price}>${pizza.prices[size]}</span>
+      <span className={styles.price}>${price}</span>
       <p className={styles.desc}>{pizza.desc}</p>
       <h3 className={styles.choose}>Choose your size</h3>
       <div className={styles.sizes}>
@@ -25,10 +38,14 @@ export default function Right({ styles, pizza }) {
       </div>
       <h3 className={styles.choose}>choose additional ingredient</h3>
       <div className={styles.ingredients}>
-        <Box id="double" styles={styles} name="Double Ingredients" />
-        <Box id="cheese" styles={styles} name="Extra Cheese" />
-        <Box id="spicy" styles={styles} name="Spicy Sauce" />
-        <Box id="garlic" styles={styles} name="Garlic Sauce" />
+        {pizza.extraOption.map((option, index) => (
+          <Box
+            key={index}
+            styles={styles}
+            option={option}
+            onChange={handleChange}
+          />
+        ))}
       </div>
       <div className={styles.add}>
         <input type="number" defaultValue={1} className={styles.quantity} />
