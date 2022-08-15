00e341d679cd661c1486347f6386b9b3ff730dc1
MoAyoub <medayb45esi@gmail.com>
2022-08-15 19:42:38 +0100

    handle order

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -1,23 +1,26 @@
 import Connect from "../../../utls/mongo";
 import Order from "../../../models/Order";
 export default async function handler(req, res) {
-  const { method } = req;
-  Connect();
-  if (method === "GET") {
-    try {
-      const product = await Product.find();
-      res.status(200).json(product);
-    } catch (ex) {
-      res.status(500).json(product);
+    const { method } = req;
+    Connect();
+    if (method === "GET") {
+        try {
+            const orders = await Order.find();
+            res.status(200).json(orders);
+        } catch (ex) {
+            res.status(500).json({ error: ex });
+        }
     }
-  }
-  if (method === "POST") {
-    try {
-      const product = await Product.create(req.body);
-      res.status(201).json(product);
-    } catch (ex) {
-      res.status(500).json(ex);
+    else if (method === "POST") {
+        try {
+            const order = await Order.create(req.body);
+            res.status(201).json(order);
+        } catch (ex) {
+            res.status(500).json({ error: ex });
+        }
     }
-  }
+    else return await res.status(404).send("Page not found...");
 }
+
+
 
