4202259bd0f74e22e904db9e3bf4734e41d1935e
MoAyoub <medayb45esi@gmail.com>
2022-08-17 22:01:29 +0100

    delete req

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -20,8 +20,12 @@ export default async function handler(req, res) {
 
       break;
     case "DELETE":
-      console.log("Nothing ..");
-
+      try {
+        const product = await Product.findByIdAndDelete(id);
+        res.status(200).json("The Product has been Deleted");
+      } catch (ex) {
+        console.error(ex);
+      }
       break;
     default:
       res.status(404).send("Error 404 Page Not Found !");
